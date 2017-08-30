8654cb30919d7180fd6aad73a4ceb943d74aac94
PauloJorgeJunior <p4ulojorge@gmail.com>
2017-08-30 17:31:23 -0400

    Aula 7.3

--- src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryImpl.java
+++ src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryImpl.java
@@ -16,9 +16,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
+import com.example.algamoney.api.model.Categoria_;
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.model.Lancamento_;
+import com.example.algamoney.api.model.Pessoa_;
 import com.example.algamoney.api.repository.filter.LancamentoFilter;
+import com.example.algamoney.api.repository.projection.ResumoLancamento;
 
 public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery{
 
@@ -40,6 +43,58 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery{
 		return new PageImpl<>(query.getResultList(),pageable,total(lancamentoFilter));
 	}
 
+	public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable) {
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		CriteriaQuery<ResumoLancamento> criteria = builder.createQuery(ResumoLancamento.class);
+		Root<Lancamento> root = criteria.from(Lancamento.class);
+		
+		criteria.select(builder.construct(ResumoLancamento.class
+				, root.get(Lancamento_.codigo), root.get(Lancamento_.descricao)
+				, root.get(Lancamento_.dataVencimento), root.get(Lancamento_.dataPagamento)
+				, root.get(Lancamento_.valor), root.get(Lancamento_.tipo)
+				, root.get(Lancamento_.categoria).get(Categoria_.nome)
+				, root.get(Lancamento_.pessoa).get(Pessoa_.nome)));
+		
+		Predicate[] predicates = criarRestricoes(lancamentoFilter, builder, root);
+		criteria.where(predicates);
+		
+		TypedQuery<ResumoLancamento> query = manager.createQuery(criteria);
+		adicionarRestricoesDePaginacao(query, pageable);
+		
+		return new PageImpl<>(query.getResultList(), pageable, total(lancamentoFilter));
+	}
+
+	private Predicate[] criarRestricoes(LancamentoFilter lancamentoFilter, CriteriaBuilder builder,
+			Root<Lancamento> root) {
+		List<Predicate> predicates = new ArrayList<>();
+		
+		if (!StringUtils.isEmpty(lancamentoFilter.getDescricao())) {
+			predicates.add(builder.like(
+					builder.lower(root.get(Lancamento_.descricao)), "%" + lancamentoFilter.getDescricao().toLowerCase() + "%"));
+		}
+		
+		if (lancamentoFilter.getDataVencimentoDe() != null) {
+			predicates.add(
+					builder.greaterThanOrEqualTo(root.get(Lancamento_.dataVencimento), lancamentoFilter.getDataVencimentoDe()));
+		}
+		
+		if (lancamentoFilter.getDataVencimentoAte() != null) {
+			predicates.add(
+					builder.lessThanOrEqualTo(root.get(Lancamento_.dataVencimento), lancamentoFilter.getDataVencimentoAte()));
+		}
+		
+		return predicates.toArray(new Predicate[predicates.size()]);
+	}
+
+	private void adicionarRestricoesDePaginacao(TypedQuery<?> query, Pageable pageable) {
+		int paginaAtual = pageable.getPageNumber();
+		int totalRegistrosPorPagina = pageable.getPageSize();
+		int primeiroRegistroDaPagina = paginaAtual * totalRegistrosPorPagina;
+		
+		query.setFirstResult(primeiroRegistroDaPagina);
+		query.setMaxResults(totalRegistrosPorPagina);
+	}
+	
 	private Long total(LancamentoFilter lancamentoFilter) {
 		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		CriteriaQuery<Long> criteria = builder.createQuery(Long.class);
@@ -51,32 +106,5 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery{
 		criteria.select(builder.count(root));
 		return manager.createQuery(criteria).getSingleResult();
 	}
-
-	private void adicionarRestricoesDePaginacao(TypedQuery<Lancamento> query, Pageable pageable) {
-		int paginaAtual = pageable.getPageNumber();
-		int totalRegistrosPorPagina = pageable.getPageSize();
-		int primeiroRegistroDaPagina = paginaAtual * totalRegistrosPorPagina;
-		
-		query.setFirstResult(primeiroRegistroDaPagina);
-		query.setMaxResults(totalRegistrosPorPagina);
-	}
-
-	private Predicate[] criarRestricoes(LancamentoFilter lancamentoFilter, CriteriaBuilder builder,
-			Root<Lancamento> root) {
-		List<Predicate> predicates = new ArrayList<>();
-		
-		if(!StringUtils.isEmpty(lancamentoFilter.getDescricao())){
-			predicates.add(builder.like(builder.lower(root.get(Lancamento_.descricao)), "%" + lancamentoFilter.getDescricao().toLowerCase() + "%"));
-		}
-		
-		if(lancamentoFilter.getDataVencimentoDe() != null){
-			predicates.add(builder.greaterThanOrEqualTo(root.get(Lancamento_.dataVencimento), lancamentoFilter.getDataVencimentoDe()));
-		}
-		
-		if(lancamentoFilter.getDataVencimentoAte() != null){
-			predicates.add(builder.lessThanOrEqualTo(root.get(Lancamento_.dataVencimento), lancamentoFilter.getDataVencimentoAte()));
-		}
-		return predicates.toArray(new Predicate[predicates.size()]);
-	}
-
+	
 }
--- target/classes/db/migration/V04__criar_e_registrar_usuarios_e_permissoes.sql
+++ target/classes/db/migration/V04__criar_e_registrar_usuarios_e_permissoes.sql
@@ -1,5 +1,5 @@
 CREATE SEQUENCE usuario_codigo_seq;
-CREATE SEQUENCE permissao_codigo_seq;
+
 
 CREATE TABLE usuario (
 	codigo bigint PRIMARY KEY NOT NULL DEFAULT nextval('usuario_codigo_seq'),
@@ -8,17 +8,16 @@ CREATE TABLE usuario (
 	senha VARCHAR(150) NOT NULL
 );
 
+CREATE SEQUENCE permissao_codigo_seq;
+
 CREATE TABLE permissao (
 	codigo bigint PRIMARY KEY NOT NULL DEFAULT nextval('permissao_codigo_seq'),
-	descricao VARCHAR(50) NOT NULL,
+	descricao VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE usuario_permissao (
 	codigo_usuario bigint NOT NULL,
 	codigo_permissao bigint NOT NULL,
-	nome VARCHAR(50) NOT NULL,
-	email VARCHAR(50) NOT NULL,
-	senha VARCHAR(150) NOT NULL
 	PRIMARY KEY (codigo_usuario,codigo_permissao),
 	FOREIGN KEY (codigo_usuario) REFERENCES usuario(codigo),
 	FOREIGN KEY (codigo_permissao) REFERENCES permissao(codigo)
